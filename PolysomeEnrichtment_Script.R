b82765d7415b73ec07c01a6b40b1fd20b1091d2b
FranziskaEPeter <franziskaelisabeth.peter@gmx.de>
2024-10-09 14:59:02 +0200

    Changed directories because "Omics Data" folder was re-named to "Omics". Created STAR index with new downloaded Annotation files. Performed STAR mapping. Performed quality control with MultiQC.

--- PolysomeEnrichtment_Script.R
+++ PolysomeEnrichtment_Script.R
@@ -8,7 +8,7 @@
 
 #Concatenate files from different lanes into single file
 #Rename files to a uniform format if not already done (eg: Experiment_Sample_Treatment_Replicate_Mate -> NAA_90min_Ctrl_1_1.fastq.gz)
-#setwd("Y:/Omics Data/RiboSeq/PolysomeEnrichment/RawData/cleaned)
+#setwd("Y:/Omics/RiboSeq/PolysomeEnrichment/RawData/cleaned)
 #file.rename(dir(), gsub("-MACE_D.*Seq", "",dir()))
 #Only work with gzipped files for more efficient storage
 #Keep all files in a single folder
@@ -17,6 +17,51 @@
 # QUALITY CONTROL _____________________________________________________________________________________________
 
 #Run FastQC on the read files
-setwd("Y:/Omics Data/RiboSeq/PolysomeEnrichment/RawData/cleaned/") #switch to directory containing the reads
+setwd("Y:/Omics/RiboSeq/PolysomeEnrichment/RawData/cleaned/") #switch to directory containing the reads
 system(paste("wsl fastqc *.fastq.gz --outdir ../FastQC/")) #Run FastQC over all files and save results to specified folder
 
+# SET DIRECTORIES _____________________________________________________________________________________________
+
+#Set linux path to genome directory
+genomeDir <- "/mnt/y/Omics/RiboSeq/PolysomeEnrichment/Annotations/STAR_Index"
+#Set linux path to genome fasta file
+genomeFastaFiles <- "/mnt/y/Omics/RiboSeq/PolysomeEnrichment/Annotations/TAIR10_chr_all.fas"
+#Set linux path to annotation GTF file
+GTFfile <- "/mnt/y/Omics/RiboSeq/PolysomeEnrichment/Annotations/Araport11_GTF_genes_transposons.20241001.gtf"
+
+# STAR MAPPING ################################################################################################
+
+#Generate Reference Genome using R to construct a command for the windows command line which in turn calls the linux subsystem (wsl)
+system(paste("wsl ~/STAR-2.7.11b/source/STAR",
+             "--runThreadN 30", #number of threads to use for computation
+             "--runMode genomeGenerate", #run mode to generate index
+             "--genomeDir", genomeDir, #directory in which to store the index (see above)
+             "--genomeFastaFiles", genomeFastaFiles, #directory of the genome fasta (see above)
+             "--sjdbGTFfile", GTFfile, #directory of the annotation (see above)
+             "--sjdbOverhang 64", # max read length - 1
+             "--genomeSAindexNbases 12")) #length of SA pre-indexing string, scaled to Arabidopsis genome according to manual: min(14, log2(GenomeLength)/2 - 1)
+
+#Run STAR mapping in single-end mode 
+setwd("Y:/Omics/RiboSeq/PolysomeEnrichment/RawData/cleaned/") #switch to directory containing the reads
+
+for (k in list.files(pattern = ".fastq.gz$")) { 
+  system(paste0("wsl echo \"Processing ", k, "\"; ~/STAR-2.7.11b/source/STAR ",
+                "--readFilesCommand zcat ",
+                "--genomeDir ", genomeDir, " ",
+                "--readFilesIn ", k, " ",
+                "--outFileNamePrefix ~/STAR_Output/", gsub("fastq\\.gz$", "", k), " ",
+                "--runThreadN 30 ",
+                "--quantMode GeneCounts"))
+  print("Moving STAR Output from Linux filesystem to Windows...")
+  system("wsl mv ~/STAR_Output/* /mnt/y/Omics/RiboSeq/PolysomeEnrichment/STAR_Output/")
+  print("Done")
+}
+
+setwd("Y:/Omics/RiboSeq/PolysomeEnrichment") #switch back to original working directory
+
+#MultiQC #####################################################################################################
+
+#run MultiQC to get a summarised report
+#run in Ubuntu shell because it's not working with the system("wsl ...") command,  
+# cd /mnt/y/Omics/RiboSeq/PolysomeEnrichment/ #set working directory
+# multiqc . -o MultiQC/ #performing MultiQC analysis with data from working directory, MultiQC report will be saved in the defined output directory 
